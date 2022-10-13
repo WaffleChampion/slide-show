89921f07c3feea36abaa6fb901eb4f5661973205
Kornelia Arnet Lyberg <104372863+KorneliaAL@users.noreply.github.com>
2022-10-13 13:52:42 +0200

    Cleaned code

--- js/script.js
+++ js/script.js
@@ -1,19 +1,21 @@
-const images = document.querySelectorAll('.slide-show__image');
+const images = document.querySelectorAll('.slide-show__image-box');
 const nextButton = document.querySelector('.slide-show__next-image');
-const previousButton = document.querySelector('.slide-show__previous-image')
+const previousButton = document.querySelector('.slide-show__previous-image');
 //const goToImageButton = document.querySelectorAll('.image-button__go-to')
-const goToImage1 = document.querySelector('#btn1')
-const goToImage2 = document.querySelector('#btn2')
-const goToImage3 = document.querySelector('#btn3')
-const goToImage4 = document.querySelector('#btn4')
-const goToImage5 = document.querySelector('#btn5')
-const goToImage6 = document.querySelector('#btn6')
-const goToImage7 = document.querySelector('#btn7')
-const goToImage8 = document.querySelector('#btn8')
-const goToImage9 = document.querySelector('#btn9')
-const goToImage10 = document.querySelector('#btn10')
+const goToImage1 = document.querySelector('#btn1');
+const goToImage2 = document.querySelector('#btn2');
+const goToImage3 = document.querySelector('#btn3');
+const goToImage4 = document.querySelector('#btn4');
+const goToImage5 = document.querySelector('#btn5');
+const goToImage6 = document.querySelector('#btn6');
+const goToImage7 = document.querySelector('#btn7');
+const goToImage8 = document.querySelector('#btn8');
+const goToImage9 = document.querySelector('#btn9');
+const goToImage10 = document.querySelector('#btn10');
 
 let currentImage = 0;
+
+const toggleImagesVisible = images[currentImage].classList.toggle('slide-show__image-box--visible');
 
 nextButton.addEventListener('click', handleNextBtnClick);
 previousButton.addEventListener('click', handlePrevBtnClick);
@@ -31,28 +33,28 @@ goToImage10.addEventListener('click', handleGoToImageClick)
 
 window.addEventListener('keyup', handleKeyUp)
 
-images[currentImage].classList.toggle('slide-show__image--visible')
+toggleImagesVisible;
 
 function handleNextBtnClick() {
-	images[currentImage].classList.toggle('slide-show__image--visible');
+	toggleImagesVisible;
 	if((currentImage+1)===images.length){
 		currentImage = 0;
-		images[currentImage].classList.toggle('slide-show__image--visible')
+		toggleImagesVisible;
 	}else{
 		currentImage += 1;
-		images[currentImage].classList.toggle('slide-show__image--visible')
+		toggleImagesVisible;
 	}
 }
 
 
 function handlePrevBtnClick() {
-	images[currentImage].classList.toggle('slide-show__image--visible');
+	toggleImagesVisible;
 	if(currentImage===0){
 		currentImage = (images.length-1);
-		images[currentImage].classList.toggle('slide-show__image--visible')
+		toggleImagesVisible;
 	}else{
 		currentImage -= 1;
-		images[currentImage].classList.toggle('slide-show__image--visible')
+		toggleImagesVisible;
 	}
 	
 }
@@ -60,47 +62,47 @@ function handlePrevBtnClick() {
 function handleGoToImageClick(event) {
 	console.log(event.currentTarget.id);
 	let currentTarget = event.currentTarget;
-	images[currentImage].classList.toggle('slide-show__image--visible');
+	toggleImagesVisible;
 	switch (currentTarget.id) {
 		case 'btn1':
 			currentImage=0;
-			images[currentImage].classList.toggle('slide-show__image--visible')
+			toggleImagesVisible;
 			break;
 		case 'btn2':
 			currentImage=1;
-			images[currentImage].classList.toggle('slide-show__image--visible');
+			toggleImagesVisible;
 			break;
 		case 'btn3':
 			currentImage=2
-			images[currentImage].classList.toggle('slide-show__image--visible')
+			toggleImagesVisible;
 		break;
 		case 'btn4':
 			currentImage=3
-			images[currentImage].classList.toggle('slide-show__image--visible')
+			toggleImagesVisible;
 		break;
 		case 'btn5':
 			currentImage=4
-			images[currentImage].classList.toggle('slide-show__image--visible')
+			toggleImagesVisible;
 		break;
 		case 'btn6':
 			currentImage=5
-			images[currentImage].classList.toggle('slide-show__image--visible')
+			toggleImagesVisible;
 		break;
 		case 'btn7':
 			currentImage=6
-			images[currentImage].classList.toggle('slide-show__image--visible')
+			toggleImagesVisible;
 		break;
 		case 'btn8':
 			currentImage=7
-			images[currentImage].classList.toggle('slide-show__image--visible')
+			toggleImagesVisible;
 		break;
 		case 'btn9':
 			currentImage=8
-			images[currentImage].classList.toggle('slide-show__image--visible')
+			toggleImagesVisible;
 		break;
 		case 'btn10':
 			currentImage=9
-			images[currentImage].classList.toggle('slide-show__image--visible')
+			toggleImagesVisible;
 		break;
 		default:
 
@@ -111,25 +113,24 @@ function handleGoToImageClick(event) {
 
 function handleKeyUp(event) {
 	if(event.key==='ArrowLeft' || event.key==='ArrowRight'){
-		images[currentImage].classList.toggle('slide-show__image--visible');
+		toggleImagesVisible;;
 		if(event.key==='ArrowLeft'){
 			if(currentImage===0){
 				currentImage = (images.length-1);
-				images[currentImage].classList.toggle('slide-show__image--visible')
+				toggleImagesVisible;
 			}else{
 				currentImage -= 1;
-				images[currentImage].classList.toggle('slide-show__image--visible')
+				toggleImagesVisible;
 			}
 			
 		}else if(event.key==='ArrowRight'){
 			if((currentImage+1)===images.length){
 				currentImage = 0;
-				images[currentImage].classList.toggle('slide-show__image--visible')
+				toggleImagesVisible;
 			}else{
 				currentImage += 1;
-				images[currentImage].classList.toggle('slide-show__image--visible')
+				toggleImagesVisible;
 			}
-			
 		}
 	}
 }
