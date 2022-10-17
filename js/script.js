272561056d7e660cf4f60fa18e336713733f96ba
WaffleChampion <bendikmdelp@gmail.com>
2022-10-17 14:31:27 +0200

    implemented mouse click on image

--- js/script.js
+++ js/script.js
@@ -2,43 +2,55 @@ const images = document.querySelectorAll('.slide-show__image-box');
 const nextButton = document.querySelector('.slide-show__next-image');
 const previousButton = document.querySelector('.slide-show__previous-image');
 const goToImageButton = document.querySelectorAll('.image-button__go-to')
-/* const goToImage1 = document.querySelector('#btn1');
-const goToImage2 = document.querySelector('#btn2');
-const goToImage3 = document.querySelector('#btn3');
-const goToImage4 = document.querySelector('#btn4');
-const goToImage5 = document.querySelector('#btn5');
-const goToImage6 = document.querySelector('#btn6');
-const goToImage7 = document.querySelector('#btn7');
-const goToImage8 = document.querySelector('#btn8');
-const goToImage9 = document.querySelector('#btn9');
-const goToImage10 = document.querySelector('#btn10'); */
 
 let currentImage = 0;
 
 const toggleImageVisible = 'slide-show__image-box--visible';
 
-nextButton.addEventListener('click', handleNextBtnClick);
-previousButton.addEventListener('click', handlePrevBtnClick);
-goToImageButton.forEach(button =>{
-	button.addEventListener('click', handleGoToImageClick)
+nextButton.addEventListener('click', handleNextbuttonClick);
+previousButton.addEventListener('click', handlePrevbuttonClick);
+images.forEach(image => {
+	image.addEventListener('mouseup', handleMouseUp)
+	//image.addEventListener('click', handleNextbuttonClick)
 })
 
-/* goToImage1.addEventListener('click', handleGoToImageClick)
-goToImage2.addEventListener('click', handleGoToImageClick)
-goToImage3.addEventListener('click', handleGoToImageClick)
-goToImage4.addEventListener('click', handleGoToImageClick)
-goToImage5.addEventListener('click', handleGoToImageClick)
-goToImage6.addEventListener('click', handleGoToImageClick)
-goToImage7.addEventListener('click', handleGoToImageClick)
-goToImage8.addEventListener('click', handleGoToImageClick)
-goToImage9.addEventListener('click', handleGoToImageClick)
-goToImage10.addEventListener('click', handleGoToImageClick) */
+goToImageButton.forEach(button => {
+	button.addEventListener('click', handleGoToImageClick)
+})
 
 window.addEventListener('keyup', handleKeyUp)
 
 images[currentImage].classList.toggle(toggleImageVisible);;
 
-function handleNextBtnClick() {
+function handleMouseUp (event) {
+	let style = getComputedStyle(event.currentTarget.firstElementChild)
+	let height= parseInt(style.height)
+	let width = parseInt(style.width)
+	if(event.offsetX >= 0 && event.offsetX <= width && event.offsetY >= 0 && event.offsetY <= height){
+		images[currentImage].classList.toggle(toggleImageVisible);;
+		if(event.offsetX >= (width/2) && event.offsetX <= width){
+			if((currentImage+1)===images.length){
+				currentImage = 0;
+				images[currentImage].classList.toggle(toggleImageVisible);;
+			}else{
+				currentImage += 1;
+				images[currentImage].classList.toggle(toggleImageVisible);;
+			}
+			
+		}else if(event.offsetX <= (width/2) && event.offsetX >=0) {
+			if(currentImage===0){
+				currentImage = (images.length-1);
+				images[currentImage].classList.toggle(toggleImageVisible);;
+			}else{
+				currentImage -= 1;
+				images[currentImage].classList.toggle(toggleImageVisible);;
+			}
+	}
+}
+
+}
+
+function handleNextbuttonClick() {
 	images[currentImage].classList.toggle(toggleImageVisible);;
 	if((currentImage+1)===images.length){
 		currentImage = 0;
@@ -50,7 +62,7 @@ function handleNextBtnClick() {
 }
 
 
-function handlePrevBtnClick() {
+function handlePrevbuttonClick() {
 	images[currentImage].classList.toggle(toggleImageVisible);;
 	if(currentImage===0){
 		currentImage = (images.length-1);
@@ -66,49 +78,49 @@ function handleGoToImageClick(event) {
 	let currentTarget = event.currentTarget;
 	images[currentImage].classList.toggle(toggleImageVisible);;
 	switch (currentTarget.id) {
-		case 'btn1':
+		case 'button1':
 			currentImage=0;
 			images[currentImage].classList.toggle(toggleImageVisible);;
 			break;
-		case 'btn2':
+		case 'button2':
 			currentImage=1;
 			images[currentImage].classList.toggle(toggleImageVisible);;
 			break;
-		case 'btn3':
+		case 'button3':
 			currentImage=2
 			images[currentImage].classList.toggle(toggleImageVisible);;
 		break;
-		case 'btn4':
+		case 'button4':
 			currentImage=3
 			images[currentImage].classList.toggle(toggleImageVisible);;
 		break;
-		case 'btn5':
+		case 'button5':
 			currentImage=4
 			images[currentImage].classList.toggle(toggleImageVisible);;
 		break;
-		case 'btn6':
+		case 'button6':
 			currentImage=5
 			images[currentImage].classList.toggle(toggleImageVisible);;
 		break;
-		case 'btn7':
+		case 'button7':
 			currentImage=6
 			images[currentImage].classList.toggle(toggleImageVisible);;
 		break;
-		case 'btn8':
+		case 'button8':
 			currentImage=7
 			images[currentImage].classList.toggle(toggleImageVisible);;
 		break;
-		case 'btn9':
+		case 'button9':
 			currentImage=8
 			images[currentImage].classList.toggle(toggleImageVisible);;
 		break;
-		case 'btn10':
+		case 'button10':
 			currentImage=9
 			images[currentImage].classList.toggle(toggleImageVisible);;
 		break;
 		default:
 
-			break;
+		break;
 	}
 
 }
